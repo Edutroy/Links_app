11339cf129533909d0a1ac6b0be3e79a8dc800a8
Gundam Taicho <lbeduardo8@gmail.com>
2023-05-02 20:07:28 -0500

    Proteccion de Rutas y Datos privados completado

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,20 +1,48 @@
 const express =require('express');
 const router= express.Router();
 const passport =  require('passport');
+const { isLoggedIn,isNotLoggedIn } = require('../lib/auth');
 
-router.get ('/signup', (req,res) =>{
+router.get ('/signup',isNotLoggedIn ,(req,res) =>{
 res.render('auth/signup');
 ;})
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup',isNotLoggedIn, passport.authenticate('local.signup', {
     successRedirect: '/profile',
     failureRedirect: '/signup',
     failureFlash: true
 }
-))
+));
 
-router.get('/profile', (req, res) => {
-    res.send('this is your profile');
+router.get('/signin',isNotLoggedIn, (req, res) => {
+  res.render('auth/signin');
+});
+
+router.post('/signin', isNotLoggedIn,(req, res, next) => {
+  /* req.check('username', 'Username is Required').notEmpty();
+  req.check('password', 'Password is Required').notEmpty();
+  const errors = req.validationErrors();
+  if (errors.length > 0) {
+    req.flash('message', errors[0].msg);
+    res.redirect('/signin');
+  } */
+  passport.authenticate('local.signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+    failureFlash: true
+  })(req, res, next);
+});
+
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile');
   });
+
+
+  router.get("/logout",isLoggedIn, (req, res, next) => {
+    req.logOut(req.user, err => {
+        if(err) return next(err);
+        res.redirect("/signin");  
+    });
+});
 
 module.exports =router;
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,45 +1,47 @@
 const express =require('express');
 const router= express.Router();
 const pool = require ('../database');
+const { isLoggedIn } = require('../lib/auth');
 
-router.get ('/add' , (req,res)=>{
+router.get ('/add' ,isLoggedIn, (req,res)=>{
 res.render('links/add');    
 });
 
-router.post('/add', async (req, res)=>{
+router.post('/add',isLoggedIn, async (req, res)=>{
     const { title,url,description }= req.body;//obtiene las propiedades del body
     const newLink ={//guarda los datos en un nuevo objeto 'newLink'
         title,
         url,
-        description
-    };
+        description,
+        user_id: req.user.id 
+        };
     await pool.query('INSERT INTO links set ?',[newLink]);
     req.flash('success','Link saved succesfully');
     res.redirect('/links');
 });
 
-router.get ('/', async (req, res)=>{
-    const links = await pool.query('SELECT * FROM links');
+router.get ('/',isLoggedIn, async (req, res)=>{
+    const links = await pool.query('SELECT * FROM links Where user_id = ?',[req.user.id]);
     console.log(links);
     res.render("links/list", {links});
     
 })
 
-router.get('/delete/:id', async (req, res) => {
+router.get('/delete/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM links WHERE ID = ?', [id]);
     req.flash('success', 'Link Removed Successfully');
     res.redirect('/links');
 });
 
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
-    console.log(links);
+    
     res.render('links/edit', {link: links[0]});
 });
 
-router.post('/edit/:id', async (req, res) => {
+router.post('/edit/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const { title, description, url} = req.body; 
     const newLink = {
