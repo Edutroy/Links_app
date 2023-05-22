159058502fb82662744181fbcfeead15516f19d4
Gundam Taicho <lbeduardo8@gmail.com>
2023-05-21 19:09:56 -0500

    Listo para deploy

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,21 @@
+const PORT = process.env.PORT || 3000
+
+ const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER = process.env.DB_USER || 'root'
+ const DB_PASSWORD = process.env.DB_PASSWORD || ''
+ const DB_NAME = process.env.DB_NAME || 'database_links'
+
 module.exports= {
 
 database : {
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'database_links'
+    host:DB_HOST,
+    user:DB_USER,
+    password:DB_PASSWORD,
+    database:DB_NAME
 }
 
 }
+
+
+/* export const DB_PORT = process.env.DB_PORT || 3306
+ */
